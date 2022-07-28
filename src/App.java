0e9ccf843daea3397bb1b3e7d19e4640966b6119
Shayna Gaulden <shayna.gaulden@gmail.com>
2022-07-28 13:47:23 -0700

    connection to database success

--- src/App.java
+++ src/App.java
@@ -1,21 +1,17 @@
 import com.mongodb.client.MongoDatabase; 
 import com.mongodb.MongoClient; 
-import com.mongodb.MongoCredential;  
 public class App { 
    
    public static void main( String args[] ) {  
       
-      // Creating a Mongo client 
-      MongoClient mongo = new MongoClient( "localhost" , 27017 ); 
-   
-      // Creating Credentials 
-      MongoCredential credential; 
-      credential = MongoCredential.createCredential("sampleUser", "myDb", 
-         "password".toCharArray()); 
-      System.out.println("Connected to the database successfully");  
-      
+      // Creating a Mongo client  172-31-82-48:27021
+      MongoClient mongo = new MongoClient( "ec2-54-174-23-133.compute-1.amazonaws.com" , 27021 ); 
+
       // Accessing the database 
-      MongoDatabase database = mongo.getDatabase("myDb"); 
-      System.out.println("Credentials ::"+ credential);     
+      MongoDatabase database = mongo.getDatabase("mydbs"); 
+
+      for (String s : database.listCollectionNames()) {
+         System.out.println(s);
+      }   
    } 
 }
